17ecb520d2b25911f7ce2bfabc8d9d49700ccb3e
mmullen2 <39490555+mmullen2@users.noreply.github.com>
2018-05-22 10:25:27 -0400

    Update player.java

--- player.java
+++ player.java
@@ -2,6 +2,6 @@
 //https://clunse.deviantart.com/gallery/60110085/Ulgrim
 public class player
 {
-  private String[] abilities = {Ice Wall, Ghost Walk, 
+  private String[] abilities = {Ice Wall, Ghost Walk, Cold Snap, Alacrity, EMP, Tornado,
   
     
