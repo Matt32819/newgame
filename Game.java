9c20cee252beabf12a5e038525c613faa92067d3
Compurity <wings4free@gmail.com>
2018-05-23 09:58:13 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,16 @@
+/**
+ * AWT Sample application
+ *
+ * @author 
+ * @version 1.00 18/05/21
+ */
+public class Game {
+    
+    public static void main(String[] args) {
+        // Create application frame.
+        GameFrame frame = new GameFrame();
+        
+        // Show frame
+        frame.setVisible(true);
+    }
+}
