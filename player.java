488d7464542442e2648e70c7e5b5dc845857f780
Compurity <wings4free@gmail.com>
2018-05-24 10:02:35 -0400

    Update player.java

--- player.java
+++ player.java
@@ -1,8 +1,26 @@
 public class Player{
 	private int Health;
 	private int PDamage;
-	private int Level;
+	private int pLevel;
 	private int Mana;
 	private int Gold;
-	private int 
+	private double pAttkS;
+	
+	public Player(){
+		Health = 650;
+		PDamage = 46;
+		pLevel = 1;
+		Mana = 400;
+		Gold = 600;
+		pAttkS = 1.3;
+	}
+	public Player(int a, int b, int c, int d, int e, int f){
+		Health = a;
+		PDamage = b;
+		pLevel = c;
+		Mana = d;
+		Gold = e;
+		pAttkS = f;
+		
+	}
 }
